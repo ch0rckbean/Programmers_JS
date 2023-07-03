45bfcb4a2e0729c9fa97acb544652abfa01bf3ce
ne0n9uy <2leebins@naver.com>
2023-07-03 23:53:35 +0900

    1-3

--- 1-3.자릿수더하기.js
+++ 1-3.자릿수더하기.js
@@ -1,8 +1,13 @@
 function solution(n)
 {
-    var answer = 0;
+    var answer = 0;    
+    let arr=n.toString();
     
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    for (let i = 0; i<=arr.length; i++){
+        answer+=parseInt(arr[i]);
+    }
+
+    console.log(arr);
     console.log(answer);
     return answer;
 }
