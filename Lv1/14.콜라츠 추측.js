701c482f13ddf56bba7fcffe7e5b4019b76c34ba
ne0n9uy <2leebins@naver.com>
2023-07-30 21:02:10 +0900

    re sol

--- Lv1/14.콜라츠 추측.js
+++ Lv1/14.콜라츠 추측.js
@@ -1,19 +1,26 @@
+// while (num != 1) {
+//   if (num % 2 == 0) {
+//     num /= 2;
+//     answer += 1;
+//   } else if (num % 2 == 1) {
+//     num = num * 3 + 1;
+//     answer += 1;
+//   }
+//   if (answer == 500) {
+//     answer = -1;
+//   }
+// }
 function solution(num) {
   var answer = 0;
-  if (num == 1) {
-    return answer;
-  }
+
+  // if (num == 1) {
+  //   return answer;
+  // }
+
   while (num != 1) {
-    if (num % 2 == 0) {
-      num /= 2;
-      answer += 1;
-    } else if (num % 2 == 1) {
-      num = num * 3 + 1;
-      answer += 1;
-    }
-    if (answer == 500) {
-      answer = -1;
-    }
+    answer += 1;
+    num % 2 == 0 ? (num /= 2) : (num = num * 3 + 1);
+    if (answer == 500) return -1;
   }
   console.log(answer);
   return answer;
