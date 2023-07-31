6dc8029771fa4fe7a1273f452d765ad17fefc79e
chr0ckbean <2leebins@naver.com>
2023-07-31 12:56:09 +0900

    merge

--- Lv1/14.콜라츠 추측.js
+++ Lv1/14.콜라츠 추측.js
@@ -1,22 +1,24 @@
 function solution(num) {
   var answer = 0;
-  if (num == 1) {
-    return answer;
-  }
-  while (num != 1) {
-    if (num % 2 == 0) {
-      num /= 2;
-      answer += 1;
-    } else if (num % 2 == 1) {
-      num = num * 3 + 1;
-      answer += 1;
-    }
-    if (answer == 500) {
-      answer = -1;
-    }
-  }
+  // if (num == 1) {
+  //   return answer;
+  // }
+  // while (num != 1) {
+  //   if (num % 2 == 0) {
+  //     num /= 2;
+  //     answer += 1;
+  //   } else if (num % 2 == 1) {
+  //     num = num * 3 + 1;
+  //     answer += 1;
+  //   }
+  // }
+  // if (answer >= 500) {
+  //   answer = -1;
+  // }
+  // while 문으로 풀기
   console.log(answer);
   return answer;
 }
 solution(6);
 solution(16);
+solution(626331);
